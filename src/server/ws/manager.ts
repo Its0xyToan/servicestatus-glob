3bba08ba46e6ab5095a4554c2348936f77ac7ccd
Its0xyToan <0xytoanmail@gmail.com>
2025-11-22 22:07:00 -0500

    feat: sendGlobalEvent() method

--- src/server/ws/manager.ts
+++ src/server/ws/manager.ts
@@ -1,24 +1,33 @@
-import type { WebSocketServer } from "ws"
+import type { WebSocketServer, WebSocket } from "ws"
 import type { Logger } from "../../logger/logger.js"
+
+const connections = new Set<WebSocket>()
 
 export const setupWss = async (logger: Logger, wss: WebSocketServer) => {
     wss.on('connection', (ws) => {
-        logger.info('A new client connected');
+        connections.add(ws)
+        logger.debug('A new client connected');
 
         ws.on('message', (message) => {
             try {
                 const json = JSON.parse(message.toString());
                 if(json.code === 1) {
-                    ws.send(JSON.stringify({ ok: true, message: "Connected" }));
+                    ws.send(JSON.stringify({ code: 1, ok: true, message: "Connected" }));
                 }
 
             } catch {
-                ws.send(JSON.stringify({ error: "There was an error while parsing JSON", ok: false }));
+                ws.send(JSON.stringify({ code: 0, error: "There was an error while parsing JSON", ok: false }));
             }
         });
 
         ws.on('close', () => {
-            logger.info('A client disconnected');
+            logger.debug('A client disconnected');
         });
     });
+}
+
+export const sendGlobalEvent = (event: string, data: any) => {
+    connections.forEach((val) => {
+        val.send(JSON.stringify({ code: 2, event, data }));
+    })
 }
